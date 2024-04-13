0d83af1ff962f8f72d7120c45d4d8fa1c4925643
Rishov Chakraborty <rishon3003@gmail.com>
2024-04-13 16:03:38 +0530

    job portal done

--- Job Search/main.js
+++ Job Search/main.js
@@ -1,199 +1,244 @@
 const jobs = [
-    {
-      title: "Software Engineer",
-      image: "https://images.credly.com/images/1e3e36a0-ab85-40f7-802e-006e90211db6/Software_Engineering_GVSU_Badge.png",
-      details:
-        "Responsible for design, development, testing and maintainance of software applications",
-      openPositions: "8",
-      link: "#",
-    },
+  {
+    title: "Software Engineer",
+    image: "https://images.credly.com/images/1e3e36a0-ab85-40f7-802e-006e90211db6/Software_Engineering_GVSU_Badge.png",
+    details:
+      "Responsible for design, development, testing and maintainance of software applications",
+    openPositions: "8",
+    link: "#",
+  },
+
+  {
+    title: "Security Engineer",
+    image: "https://tse1.mm.bing.net/th?id=OIP.Ne2m5kpkMC4M0ha3Eg_e3wHaFS&pid=Api&P=0&h=180",
+    details:
+      "Responsible to build security systems and monitor security controls to protect the data from cyber attacks.",
+    openPositions: "6",
+    link: "#",
+  },
   
-    {
-      title: "Security Engineer",
-      image: "https://tse1.mm.bing.net/th?id=OIP.Ne2m5kpkMC4M0ha3Eg_e3wHaFS&pid=Api&P=0&h=180",
-      details:
-        "Responsible to build security systems and monitor security controls to protect the data from cyber attacks.",
-      openPositions: "6",
-      link: "#",
-    },
-    
-    {
-      title: "Project Manager",
-      image: "https://w7.pngwing.com/pngs/905/589/png-transparent-project-management-project-manager-project-planning-others-miscellaneous-company-text.png",
-      details:
-        "Responsible for planning, executing and closing projects on time and within budget.",
-      openPositions: "3",
-      link: "#",
-    },
-  
-    {
-      title: "Cloud Developer",
-      image: "https://static.vecteezy.com/system/resources/previews/006/030/600/original/abstract-cloud-logo-blue-shape-cloud-computing-isolated-on-white-background-usable-for-business-and-technology-logos-flat-logo-design-template-element-vector.jpg",
-      details:
-        "Responsible for designing , developing and managing cloud-based infrastructures, applications and services.",
-      openPositions: "10",
-      link: "#",
-    },
-  
-    {
-      title: "AI/ML Engineer",
-      image: "https://tse1.mm.bing.net/th?id=OIP.7Lw8JJzMIYixYMlWAEr3qgAAAA&pid=Api&P=0&h=180",
-      details:
-        "Responsible for studying data science prototypes, designing ML systems and developing ML Applications",
-      openPositions: "20",
-      link: "#",
-    },
-  
-    {
-      title: "Digital Marketing Manager",
-      image: "https://www.joomconnect.com/images/JoomConnectICONaVirtualMarketingManager.png",
-      details:
-        "Responsible for creating and executing marketing strategies to promote a company or product.",
-      openPositions: "45",
-      link: "#",
-    },
-    {
-      title: "Business Deveopment Executive",
-      image: "https://tse1.mm.bing.net/th?id=OIP.SoAFlDeFypUeKI6axeOLfwHaEK&pid=Api&P=0&h=180",
-      details:
-        "Responsible for engaging with potential partners, presenting and managing product and services.",
-      openPositions: "2",
-      link: "#",
-    },
-  
-    {
-      title: "Social Media Marketing",
-      image: "https://tse1.mm.bing.net/th?id=OIP.iGCUiwXSkxRzoQBNxgx1vgEsDI&pid=Api&P=0&h=180",
-      details:
-        "Responsible to develop and implement social media strategies to increase brand awareness and drive engagements",
-      openPositions: "5",
-      link: "#",
-    },
-  
-    {
-      title: "Full Stack Developer",
-      image: "https://tse3.mm.bing.net/th?id=OIP.JVjUDJPqhMr-GqZWPKPBVwHaEW&pid=Api&P=0&h=180",
-      details:
-        "Responsible to develop both the frontend and backend projects on some real life problems",
-      openPositions: "9",
-      link: "#",
-    },
-  
-    {
-      title: "Data Entry Executive",
-      image: "https://tse4.mm.bing.net/th?id=OIP.SjGJY_wgAI29UfgihWeAnwHaEK&pid=Api&P=0&h=180",
-      details:
-        "Responsible for data related work on videoes, audio and image files.",
-      openPositions: "2",
-      link: "#",
-    },
-  
-    {
-      title: "App Developer",
-      image: "https://global-uploads.webflow.com/5e157548d6f7910beea4e2d6/6388de389494fb579a27e4ba_app-development-logo-free.png",
-      details:
-        "Responsible to develop user friendly, low latency and interactive features for stock trading apps",
-      openPositions: "3",
-      link: "#",
-    },
-  
-    {
-      title: "Regional Sales Manager",
-      image: "https://tse3.mm.bing.net/th?id=OIP.72zNZxIVP8XgkkUXQKwJFAHaHa&pid=Api&P=0&h=180",
-      details:
-        "Responsible to build strong sales and service teams and be a major stakeholder in product evolution",
-      openPositions: "15",
-      link: "#",
-    },
-    {
-      title: "Data Scientist",
-      image: "https://tse2.mm.bing.net/th?id=OIP.FMvBDoSKPl55TBG1e_G6PAHaEo&pid=Api&P=0&h=180",
-      details:
-        "Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.",
-      openPositions: "3",
-      link: "#",
-    },
-  
-    {
-      title: "Product Manager",
-      image: "https://miro.medium.com/proxy/1*6ge3gXH51icSvin7oV-4-Q.png",
-      details:
-        "Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.",
-      openPositions: "1",
-      link: "#",
-    },
-  
-    {
-      title: "Sales Representative",
-      image: "https://tse3.mm.bing.net/th?id=OIP.CaVsWylavGeX5IICtEvxFQHaGb&pid=Api&P=0&h=180",
-      details:
-        "Responsible for reaching out to potential customers and closing sales deals.",
-      openPositions: "4",
-      link: "#",
-    },
-  ];
-  
-  const jobsHeading = document.querySelector(".jobs-list-container h2");
-  const jobsContainer = document.querySelector(".jobs-list-container .jobs");
-  const jobSearch = document.querySelector(".jobs-list-container .job-search");
-  
-  let searchTerm = "";
-  
-  if (jobs.length == 1) {
-    jobsHeading.innerHTML = `${jobs.length} Job`;
-  } else {
-    jobsHeading.innerHTML = `${jobs.length} Jobs`;
-  }
-  
-  const createJobListingCards = () => {
-    jobsContainer.innerHTML = "";
-  
-    jobs.forEach((job) => {
-      if (job.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-        let jobCard = document.createElement("div");
-        jobCard.classList.add("job");
-  
-        let image = document.createElement("img");
-        image.src = job.image;
-  
-        let title = document.createElement("h3");
-        title.innerHTML = job.title;
-        title.classList.add("job-title");
-  
-        let details = document.createElement("div");
-        details.innerHTML = job.details;
-        details.classList.add("details");
-  
-        let detailsBtn = document.createElement("a");
-        detailsBtn.href = job.link;
-        detailsBtn.innerHTML = "More Details";
-        detailsBtn.classList.add("details-btn");
-  
-        let openPositions = document.createElement("span");
-        openPositions.classList.add("open-positions");
-  
-        if (job.openPositions == 1) {
-          openPositions.innerHTML = `${job.openPositions} open position`;
-        } else {
-          openPositions.innerHTML = `${job.openPositions} open positions`;
-        }
-  
-        jobCard.appendChild(image);
-        jobCard.appendChild(title);
-        jobCard.appendChild(details);
-        jobCard.appendChild(detailsBtn);
-        jobCard.appendChild(openPositions);
-  
-        jobsContainer.appendChild(jobCard);
+  {
+    title: "Project Manager",
+    image: "https://w7.pngwing.com/pngs/905/589/png-transparent-project-management-project-manager-project-planning-others-miscellaneous-company-text.png",
+    details:
+      "Responsible for planning, executing and closing projects on time and within budget.",
+    openPositions: "3",
+    link: "#",
+  },
+
+  {
+    title: "Cloud Developer",
+    image: "https://static.vecteezy.com/system/resources/previews/006/030/600/original/abstract-cloud-logo-blue-shape-cloud-computing-isolated-on-white-background-usable-for-business-and-technology-logos-flat-logo-design-template-element-vector.jpg",
+    details:
+      "Responsible for designing , developing and managing cloud-based infrastructures, applications and services.",
+    openPositions: "10",
+    link: "#",
+  },
+
+  {
+    title: "AI/ML Engineer",
+    image: "https://tse1.mm.bing.net/th?id=OIP.7Lw8JJzMIYixYMlWAEr3qgAAAA&pid=Api&P=0&h=180",
+    details:
+      "Responsible for studying data science prototypes, designing ML systems and developing ML Applications",
+    openPositions: "20",
+    link: "#",
+  },
+
+  {
+    title: "Digital Marketing Manager",
+    image: "https://www.joomconnect.com/images/JoomConnectICONaVirtualMarketingManager.png",
+    details:
+      "Responsible for creating and executing marketing strategies to promote a company or product.",
+    openPositions: "45",
+    link: "#",
+  },
+  {
+    title: "Business Development Executive",
+    image: "https://tse1.mm.bing.net/th?id=OIP.SoAFlDeFypUeKI6axeOLfwHaEK&pid=Api&P=0&h=180",
+    details:
+      "Responsible for engaging with potential partners, presenting and managing product and services.",
+    openPositions: "2",
+    link: "#",
+  },
+
+  {
+    title: "Social Media Marketing",
+    image: "https://tse1.mm.bing.net/th?id=OIP.iGCUiwXSkxRzoQBNxgx1vgEsDI&pid=Api&P=0&h=180",
+    details:
+      "Responsible to develop and implement social media strategies to increase brand awareness and drive engagements",
+    openPositions: "5",
+    link: "#",
+  },
+
+  {
+    title: "Full Stack Developer",
+    image: "https://tse3.mm.bing.net/th?id=OIP.JVjUDJPqhMr-GqZWPKPBVwHaEW&pid=Api&P=0&h=180",
+    details:
+      "Responsible to develop both the frontend and backend projects on some real life problems",
+    openPositions: "9",
+    link: "#",
+  },
+
+  {
+    title: "Data Entry Executive",
+    image: "https://tse4.mm.bing.net/th?id=OIP.SjGJY_wgAI29UfgihWeAnwHaEK&pid=Api&P=0&h=180",
+    details:
+      "Responsible for data related work on videoes, audio and image files.",
+    openPositions: "2",
+    link: "#",
+  },
+
+  {
+    title: "App Developer",
+    image: "https://global-uploads.webflow.com/5e157548d6f7910beea4e2d6/6388de389494fb579a27e4ba_app-development-logo-free.png",
+    details:
+      "Responsible to develop user friendly, low latency and interactive features for stock trading apps",
+    openPositions: "3",
+    link: "#",
+  },
+
+  {
+    title: "Regional Sales Manager",
+    image: "https://tse3.mm.bing.net/th?id=OIP.72zNZxIVP8XgkkUXQKwJFAHaHa&pid=Api&P=0&h=180",
+    details:
+      "Responsible to build strong sales and service teams and be a major stakeholder in product evolution",
+    openPositions: "15",
+    link: "#",
+  },
+  {
+    title: "Data Scientist",
+    image: "https://tse2.mm.bing.net/th?id=OIP.FMvBDoSKPl55TBG1e_G6PAHaEo&pid=Api&P=0&h=180",
+    details:
+      "Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.",
+    openPositions: "3",
+    link: "#",
+  },
+
+  {
+    title: "Product Manager",
+    image: "https://miro.medium.com/proxy/1*6ge3gXH51icSvin7oV-4-Q.png",
+    details:
+      "Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.",
+    openPositions: "1",
+    link: "#",
+  },
+
+  {
+    title: "Sales Representative",
+    image: "https://tse3.mm.bing.net/th?id=OIP.CaVsWylavGeX5IICtEvxFQHaGb&pid=Api&P=0&h=180",
+    details:
+      "Responsible for reaching out to potential customers and closing sales deals.",
+    openPositions: "4",
+    link: "#",
+  },
+];
+
+const jobsHeading = document.querySelector(".jobs-list-container h2");
+const jobsContainer = document.querySelector(".jobs-list-container .jobs");
+const jobSearch = document.querySelector(".jobs-list-container .job-search");
+
+let searchTerm = "";
+
+if (jobs.length == 1) {
+  jobsHeading.innerHTML = `${jobs.length} Job`;
+} else {
+  jobsHeading.innerHTML = `${jobs.length} Jobs`;
+}
+
+const createJobListingCards = () => {
+  jobsContainer.innerHTML = "";
+
+  jobs.forEach((job) => {
+    if (job.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+      let jobCard = document.createElement("div");
+      jobCard.classList.add("job");
+
+      let image = document.createElement("img");
+      image.src = job.image;
+
+      let title = document.createElement("h3");
+      title.innerHTML = job.title;
+      title.classList.add("job-title");
+
+      let details = document.createElement("div");
+      details.innerHTML = job.details;
+      details.classList.add("details");
+
+      let detailsBtn = document.createElement("button");
+      detailsBtn.href = job.link;
+      detailsBtn.innerHTML = "More Details";
+      detailsBtn.classList.add("details-btn");
+
+      let openPositions = document.createElement("span");
+      openPositions.classList.add("open-positions");
+
+      if (job.openPositions == 1) {
+        openPositions.innerHTML = `${job.openPositions} open position`;
+      } else {
+        openPositions.innerHTML = `${job.openPositions} open positions`;
       }
-    });
-  };
-  
-  createJobListingCards();
-  
-  jobSearch.addEventListener("input", (e) => {
-    searchTerm = e.target.value;
-  
-    createJobListingCards();
+
+      jobCard.appendChild(image);
+      jobCard.appendChild(title);
+      jobCard.appendChild(details);
+      jobCard.appendChild(detailsBtn);
+      jobCard.appendChild(openPositions);
+
+      // Add event listeners for screen reader functionality
+      jobCard.addEventListener("mouseover", () => {
+        jobCard.classList.add("hover-speak")
+        speakText(`${job.title}. ${job.details}. There are ${job.openPositions} open positions.`);
+      });
+
+      jobCard.addEventListener("mouseleave", () => {
+        stopSpeaking();
+      });
+
+      jobsContainer.appendChild(jobCard);
+    }
   });
-  
+};
+
+createJobListingCards();
+
+jobSearch.addEventListener("input", (e) => {
+  searchTerm = e.target.value;
+
+  createJobListingCards();
+});
+
+// accessibility js 
+var screenReaderEnabled = false;
+
+function toggleAccessibilityMenu() {
+  var menu = document.getElementById("accessibilityMenu");
+  menu.classList.toggle("active");
+}
+
+function toggleScreenReader() {
+  screenReaderEnabled = !screenReaderEnabled;
+  var menuButton = document.getElementById("screenReaderButton");
+  if (screenReaderEnabled) {
+    speakText("Screen reader enabled");
+    menuButton.innerText = "Disable Screen Reader";
+    document.body.classList.add("screen-reader-enabled");
+    setTimeout(toggleAccessibilityMenu, 5000);
+  } else {
+    speakText("Screen reader disabled");
+    menuButton.innerText = "Enable Screen Reader";
+    document.body.classList.remove("screen-reader-enabled");
+    toggleAccessibilityMenu();
+  }
+}
+
+function speakText(text) {
+  if (screenReaderEnabled) {
+    var utterance = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(utterance);
+  }
+}
+
+function stopSpeaking() {
+  window.speechSynthesis.cancel();
+}
+
