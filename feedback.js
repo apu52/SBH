de12eccf5cc2a3810a51c9f74b68dcbf6f85bbad
Shruti Sen <senshruti2020@gmail.com>
2024-04-17 13:03:23 +0530

    voice_recognition_added_to_feed_bak_form

--- feedback.js
+++ feedback.js
@@ -69,3 +69,95 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   
   }
+
+// JS FOR ACCESSIBILITY 
+var screenReaderEnabled = false;
+var voiceRecognitionEnabled = false;
+
+function toggleAccessibilityMenu() {
+ var menu = document.getElementById("accessibilityMenu");
+ menu.classList.toggle("active");
+}
+
+function toggleScreenReader() {
+ screenReaderEnabled = !screenReaderEnabled;
+ var menuButton = document.getElementById("screenReaderButton");
+ if (screenReaderEnabled) {
+   speakText("Screen reader enabled");
+   menuButton.innerText = "Disable Screen Reader";
+   document.body.classList.add("screen-reader-enabled");
+   setTimeout(toggleAccessibilityMenu, 5000);
+ } else {
+   speakText("Screen reader disabled");
+   menuButton.innerText = "Enable Screen Reader";
+   document.body.classList.remove("screen-reader-enabled");
+   toggleAccessibilityMenu();
+ }
+}
+
+function speakText(text) {
+ if (screenReaderEnabled) {
+   var utterance = new SpeechSynthesisUtterance(text);
+   window.speechSynthesis.speak(utterance);
+ }
+}
+
+function stopSpeaking() {
+ window.speechSynthesis.cancel();
+}
+
+function toggleVoiceRecognition() {
+  voiceRecognitionEnabled = !voiceRecognitionEnabled;
+  var menuButton = document.getElementById("voiceRecognitionButton");
+  if (voiceRecognitionEnabled) {
+      startVoiceRecognition();
+      menuButton.innerText = "Disable voice recognition";
+  } else {
+      stopVoiceRecognition();
+      menuButton.innerText = "Enable voice recognition";
+      toggleAccessibilityMenu();
+  }
+}
+
+function startVoiceRecognition() {
+  if (annyang) {
+      annyang.start();
+      speakText("Voice recognition enabled");
+  }
+}
+
+function stopVoiceRecognition() {
+  if (annyang) {
+      annyang.abort();
+      speakText("Voice recognition disabled");
+  }
+}
+
+// Voice commands
+if (annyang) {
+  var commands = {
+      'enable voice recognition': toggleVoiceRecognition,
+      'disable voice recognition': toggleVoiceRecognition,
+      'submit': submit,
+      'go to feedback': focusOnfeedback,
+      'enter feedback *feedback': enterfeedback,
+  };
+
+  annyang.addCommands(commands);
+  annyang.debug(true);
+}
+
+// function submit() {
+//   var submit = document.getElementById('myButton');
+//   if (submit) {
+//     button.click(); 
+//   }
+// };
+
+function focusOnfeedback() {
+  document.getElementById('feedback-input').focus();
+}
+
+function enterfeedback(fb) {
+  document.getElementById('feedback-input').value = fb;
+}
